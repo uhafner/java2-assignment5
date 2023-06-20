065d49a83f2d307fb653c7e1f8797c6bbb7bc996
Ulli Hafner <ullrich.hafner@gmail.com>
2023-06-20 09:34:57 +0200

    Add solution for assignment 6.

--- src/main/java/edu/hm/hafner/java2/schein/GermanyPostalCode.java
+++ src/main/java/edu/hm/hafner/java2/schein/GermanyPostalCode.java
@@ -3,7 +3,7 @@ package edu.hm.hafner.java2.schein;
 /**
  * A postal code of Germany.
  */
-public class GermanyPostalCode {
+public class GermanyPostalCode extends AbstractPostalCode {
     /**
      * Creates a new {@link GermanyPostalCode}.
      *
@@ -11,5 +11,11 @@ public class GermanyPostalCode {
      *         the code to use
      */
     public GermanyPostalCode(final int code) {
+        super(Country.GERMANY, code, 1001, 100_000 - 1);
+    }
+
+    @Override
+    public String getShortForm() {
+        return String.format("%05d", getCode());
     }
 }
--- src/main/java/edu/hm/hafner/java2/schein/PostalCodeFactory.java
+++ src/main/java/edu/hm/hafner/java2/schein/PostalCodeFactory.java
@@ -6,4 +6,30 @@ package edu.hm.hafner.java2.schein;
  * @author Ullrich Hafner
  */
 public class PostalCodeFactory {
+    /**
+     * Creates a new {@link PostalCode} instance from the specified code (given in its String representation).
+     *
+     * @param code the code to create a {@link PostalCode} from
+     * @return the {@link PostalCode}
+     * @throws IllegalArgumentException if the specified code cannot be converted to a {@link PostalCode}
+     */
+    public static PostalCode createPostalCodeFrom(final String code) {
+        if (code.startsWith("DE-")) {
+            return new GermanyPostalCode(getPlainCode(code));
+        }
+        if (code.startsWith("CH-")) {
+            return new SwitzerlandPostalCode(getPlainCode(code));
+        }
+        throw new IllegalArgumentException(String.format("Kann aus dem String %s keine Postleitzahl erzeugen", code));
+    }
+
+    private static int getPlainCode(final String code) {
+        var cityCode = code.substring(code.indexOf('-') + 1);
+        try {
+            return Integer.parseInt(cityCode);
+        }
+        catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(String.format("Kann aus dem String %s keine Postleitzahl erzeugen", code));
+        }
+    }
 }
